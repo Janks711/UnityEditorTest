4f328424b53f2d2ee49363dc505ee8bf08b37e3e
Janks-MK <michael.kijanka@mightykingdom.com>
2021-05-23 10:41:54 +0930

    Added some special context menu items

--- Assets/Editor/MenuItems.cs
+++ Assets/Editor/MenuItems.cs
@@ -2,9 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 
 public class MenuItems
 {
+    // Hot key commands you can assign to menu items
+    // % - Ctrl
+    // # - Shift
+    // & - Alt
+    // LEFT/RIGHT/UP/DOWN - Arrow Keys
+    // F1...F12 - F Keys
+    // HOME, END, PGUP, PGDN
+    
     [MenuItem("Tools/PlayerPrefs/ClearPlayerPrefs %#C")]
     private static void ClearPlayerPrefs()
     {
@@ -16,10 +25,28 @@ public class MenuItems
     {
         PlayerPrefs.Save();
     }
+
+    // Add a new menu item that is accessed by right clicking an object in project view
     
-    // [MenuItem("CONTEXT/LevelScript/ResetLevel")]
-    // public static void ResetLevel()
-    // {
-    //     Debug.Log("Reset Level");
-    // }
+    [MenuItem("Assets/Load Additive Scene")]
+    public static void LoadAdditiveScene()
+    {
+        var selected = Selection.activeObject;
+        EditorSceneManager.OpenScene(AssetDatabase.GetAssetPath(selected), OpenSceneMode.Additive);
+    }
+    
+    // Adding a new menu item under assets create
+    [MenuItem("Assets/Create/Add Config")]
+    public static void AddConfiguration()
+    {
+        // Create new scriptable object for storing config
+    }
+
+    // Add a new component that is accessed by right-clicking the Level Script Component
+    
+    [MenuItem("CONTEXT/LevelScript/Add Level Config")]
+    public static void ResetLevel()
+    {
+        Debug.Log("Add level");
+    }
 }
