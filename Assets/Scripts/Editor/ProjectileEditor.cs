fde475ad3ea3247a43e8a7e766164682c75f48f3
Janks-MK <michael.kijanka@mightykingdom.com>
2021-05-23 12:01:19 +0930

    Created Custom editor class for Projectile

---
+++ Assets/Scripts/Editor/ProjectileEditor.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Projectile))]
+public class ProjectileEditor : Editor
+{
+    [DrawGizmo(GizmoType.Selected | GizmoType.NonSelected)]
+    static void DrawGizmosSelected(Projectile projectile, GizmoType gizmoType)
+    {
+        Gizmos.DrawSphere(projectile.transform.position, 0.125f);
+    }
+    
+    private void OnSceneGUI()
+    {
+        var projectile = target as Projectile;
+        var transform = projectile.transform;
+        projectile.damageRadius = Handles.RadiusHandle(
+            transform.rotation,
+            transform.position,
+            projectile.damageRadius);
+    }
+}
