7a19a02be70a33a2ca444ee1e50ffdb6b8e65d8f
Janks-MK <michael.kijanka@mightykingdom.com>
2021-05-23 12:22:36 +0930

    Added a basic debug line to show approx positon of projectile after 1 second

--- Assets/Scripts/Editor/LauncherEditor.cs
+++ Assets/Scripts/Editor/LauncherEditor.cs
@@ -1,8 +1,30 @@
 using UnityEditor;
+using UnityEngine;
 
 [CustomEditor(typeof(Launcher))]
 public class LauncherEditor : Editor
 {
+    [DrawGizmo(GizmoType.Pickable | GizmoType.Selected)]
+    static void DrawGizmosSelected(Launcher launcher, GizmoType gizmoType)
+    {
+        var offsetPosition = launcher.transform.position + launcher.offset;
+        Handles.DrawDottedLine(launcher.transform.position, offsetPosition, 3);
+        Handles.Label(offsetPosition, "Offset");
+        if (launcher.projectile != null)
+        {
+            var endPosition = offsetPosition +
+                              (launcher.transform.forward *
+                               launcher.velocity /
+                               launcher.projectile.mass);
+            using (new Handles.DrawingScope(Color.yellow))
+            {
+                Handles.DrawDottedLine(offsetPosition, endPosition, 3);
+                Gizmos.DrawWireSphere(endPosition, 0.125f);
+                Handles.Label(endPosition, "Estimated Position");
+            }
+        }
+    }
+    
     private void OnSceneGUI()
     {
         var launcher = target as Launcher;
@@ -11,5 +33,28 @@ public class LauncherEditor : Editor
             Handles.PositionHandle(
                 transform.TransformPoint(launcher.offset),
                 transform.rotation));
+        
+        // Creating a fire button that appears on the scene view
+        Handles.BeginGUI();
+        var rectMin = Camera.current.WorldToScreenPoint(
+            launcher.transform.position +
+            launcher.offset);
+        var rect = new Rect();
+        rect.xMin = rectMin.x;
+        rect.yMin = SceneView.currentDrawingSceneView.position.height -
+                    rectMin.y;
+        rect.width = 64;
+        rect.height = 18;
+        GUILayout.BeginArea(rect);
+        // Using the below function we only enable the fire button in play mode
+        using (new EditorGUI.DisabledGroupScope(!Application.isPlaying))
+        {
+            if (GUILayout.Button("Fire"))
+            {
+                launcher.Fire();
+            }
+        }
+        GUILayout.EndArea();
+        Handles.EndGUI();  
     }
 }
